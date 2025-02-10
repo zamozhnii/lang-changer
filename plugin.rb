53fddb7eb865ff39394d33f4a21e076ca77add54
ihor <zamozhnii@gmail.com>
2025-02-10 15:17:46 +0100

    fix

--- plugin.rb
+++ plugin.rb
@@ -3,28 +3,23 @@ enabled_site_setting :enable_lang_changer
 
 # Этот метод будет вызываться при загрузке страницы
 after_initialize do
-  # Добавляем хук для изменения атрибута lang
-  Discourse::Application.routes.append do
-    get "/french-forum/*path" => "french_forum#lang_change"
-  end
-
-  # Создадим контроллер для изменения lang на 'fr'
-  class ::FrenchForumController < ::ApplicationController
-    def lang_change
-      # Проверяем, является ли категория французским форумом
-      if Category.exists?(name: 'French Forum') && category_forum?
-        response.headers['Content-Language'] = 'fr'
+    # Привязываем маршрут к категории с slug "french-forum"
+    Discourse::Application.routes.append do
+      get "/c/french-forum/:id" => "french_forum#lang_change"
+    end
+  
+    # Создаем контроллер для изменения lang на 'fr'
+    class ::FrenchForumController < ::ApplicationController
+        def lang_change
+          category = Category.find_by(id: params[:id])
+          
+          if category && category.name == 'French Forum'
+            @lang = 'fr'
+          else
+            @lang = 'en'
+          end
+      
+          render body: nil
+        end
       end
-      # Продолжаем рендерить страницу с обновленным языком
-      render :nothing => true
-    end
-
-    private
-
-    def category_forum?
-      # Получаем категорию текущей страницы (предположим, что URL соответствует)
-      category = Category.find_by_slug(params[:path])
-      category.present? && category.name == 'French Forum'
-    end
   end
-end
