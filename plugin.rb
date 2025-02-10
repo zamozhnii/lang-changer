e19c0b193ca7d1fea378165e5168529aa1b66752
ihor <zamozhnii@gmail.com>
2025-02-10 15:10:51 +0100

    fix

--- plugin.rb
+++ plugin.rb
@@ -1,55 +1,30 @@
-# name: lang_changer
-# about: Change language code
-# version: 0.1
-# authors: IDW
-# url: https://github.com/zamozhnii/lang_changer_plugin.git
+# plugins/discourse-french-forum-lang/plugin.rb
+enabled_site_setting :enable_lang_changer
 
-enabled_site_setting :lang_changer_enabled
-
+# Этот метод будет вызываться при загрузке страницы
 after_initialize do
-  # Добавление в сериализатор
-  add_to_serializer(:post, :lang_for_html) do
-    object.lang_for_html
+  # Добавляем хук для изменения атрибута lang
+  Discourse::Application.routes.append do
+    get "/french-forum/*path" => "french_forum#lang_change"
   end
 
-  # Инициализация плагина
-  if SiteSetting.lang_changer_enabled
+  # Создадим контроллер для изменения lang на 'fr'
+  class ::FrenchForumController < ::ApplicationController
+    def lang_change
+      # Проверяем, является ли категория французским форумом
+      if Category.exists?(name: 'French Forum') && category_forum?
+        response.headers['Content-Language'] = 'fr'
+      end
+      # Продолжаем рендерить страницу с обновленным языком
+      render :nothing => true
+    end
 
-    import { withPluginApi } from 'discourse/lib/plugin-api';
-    import { Store } from 'discourse/lib/store';
+    private
 
-    export default {
-        name: 'lang-changer',
-
-        initialize() {
-            withPluginApi('0.8', api => {
-                api.onPageChange(() => {
-                    // Получаем текущий маршрут
-                    const currentRoute = api.getCurrentRoute();
-
-                    // Проверяем, если это страница категории
-                    if (currentRoute && currentRoute.params && currentRoute.params.category_id) {
-                    const categoryId = currentRoute.params.category_id;
-
-                    // Получаем категорию
-                    const category = Store.findBy('categories', categoryId);
-
-                    // Проверяем имя категории и меняем lang
-                    if (category) {
-                        console.log(`You are on category: ${category.name}`);
-
-                        // Меняем lang в зависимости от категории
-                        if (category.name === 'French Forum') {
-                            document.documentElement.setAttribute('lang', 'fr');
-                        } else {
-                            document.documentElement.setAttribute('lang', 'en');
-                        }
-                    }
-                    }
-                });
-            });
-        }
-    };
-
+    def category_forum?
+      # Получаем категорию текущей страницы (предположим, что URL соответствует)
+      category = Category.find_by_slug(params[:path])
+      category.present? && category.name == 'French Forum'
+    end
   end
 end
