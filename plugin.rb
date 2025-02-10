17f984c6ed608d15d742b52321eae1149186d12c
ihor <zamozhnii@gmail.com>
2025-02-10 15:27:15 +0100

    fix

--- plugin.rb
+++ plugin.rb
@@ -6,23 +6,16 @@
 enabled_site_setting :enabled
 
 after_initialize do
-  # Регистрируем хук, который изменяет HTML до загрузки страницы
-  add_to_serializer(:site, :custom_html_lang) do
-    # Получаем текущую категорию
-    category_id = nil
-    if SiteSetting.enable_category_lang
-      category_id = request.params["category_id"]
+  # Обрабатываем запрос и добавляем lang="fr" для категории French Forum
+  on(:before_render) do |controller|
+    # Проверяем, если это категория French Forum (предположим, что ID категории = 10)
+    if controller.params[:category_id]
+      category = Category.find_by(id: controller.params[:category_id])
+      
+      if category && category.name == 'French Forum'
+        # Добавляем lang="fr" до загрузки страницы
+        controller.response.headers["X-Content-Language"] = "fr"
+      end
     end
-    
-    # Проверяем, если это категория French Forum (предположим, ID категории - 10)
-    if category_id && Category.find(category_id).name == "French Forum"
-      # Меняем lang="fr" на уровне HTML до загрузки страницы
-      html_lang = 'fr'
-    else
-      html_lang = 'en'
-    end
-
-    # Возвращаем установленный атрибут lang
-    html_lang
   end
 end
